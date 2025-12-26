956c4117fbf6f89c668193a8dec4e3dbbd5f9fd9
PoppyPop <skytep@gmail.com>
2025-12-26 10:21:31 +0000

    Fix option editor

--- custom_components/integration_ecowitt/config_flow.py
+++ custom_components/integration_ecowitt/config_flow.py
@@ -92,7 +92,7 @@ class EcowittOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize HASS options flow."""
-        self.config_entry = config_entry
+        self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Handle a flow initialized by the user."""
@@ -103,35 +103,35 @@ class EcowittOptionsFlowHandler(config_entries.OptionsFlow):
             {
                 vol.Optional(
                     CONF_UNIT_BARO,
-                    default=self.config_entry.options.get(
+                    default=self._config_entry.options.get(
                         CONF_UNIT_BARO,
                         self.hass.config.units.pressure_unit,
                     ),
                 ): vol.In(UNIT_BARO_OPTS),
                 vol.Optional(
                     CONF_UNIT_WIND,
-                    default=self.config_entry.options.get(
+                    default=self._config_entry.options.get(
                         CONF_UNIT_WIND,
                         self.hass.config.units.wind_speed_unit,
                     ),
                 ): vol.In(UNIT_WIND_OPTS),
                 vol.Optional(
                     CONF_UNIT_RAIN,
-                    default=self.config_entry.options.get(
+                    default=self._config_entry.options.get(
                         CONF_UNIT_RAIN,
                         self.hass.config.units.accumulated_precipitation_unit,
                     ),
                 ): vol.In(UNIT_PRECI_OPTS),
                 vol.Optional(
                     CONF_UNIT_LIGHTNING,
-                    default=self.config_entry.options.get(
+                    default=self._config_entry.options.get(
                         CONF_UNIT_LIGHTNING,
                         self.hass.config.units.length_unit,
                     ),
                 ): vol.In(UNIT_LENGTH_OPTS),
                 vol.Optional(
                     CONF_UNIT_WINDCHILL,
-                    default=self.config_entry.options.get(
+                    default=self._config_entry.options.get(
                         CONF_UNIT_WINDCHILL,
                         W_TYPE_HYBRID,
                     ),
